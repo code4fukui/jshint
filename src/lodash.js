ea7538f26054e1f7b704ab3a0fe1d916579f595e
Taisuke Fukuno <fukuno@jig.jp>
2023-01-24 19:31:41 +0900

    add lodash functions

--- src/lodash.js
+++ src/lodash.js
@@ -3,6 +3,12 @@ import each from "https://code4fukui.github.io/lodash/each.js";
 import clone from "https://code4fukui.github.io/lodash/clone.js";
 import isEmpty from "https://code4fukui.github.io/lodash/isEmpty.js";
 import values from "https://code4fukui.github.io/lodash/values.js";
+import includes from "https://code4fukui.github.io/lodash/includes.js";
+import reject from "https://code4fukui.github.io/lodash/reject.js";
+import isNumber from "https://code4fukui.github.io/lodash/isNumber.js";
+import zip from "https://code4fukui.github.io/lodash/zip.js";
+import some from "https://code4fukui.github.io/lodash/slice.js";
+import findLastIndex from "https://code4fukui.github.io/lodash/findLastIndex.js";
 
 export default {
   has,
@@ -11,4 +17,10 @@ export default {
   clone,
   isEmpty,
   values,
+  includes,
+  reject,
+  isNumber,
+  zip,
+  some,
+  findLastIndex,
 };
